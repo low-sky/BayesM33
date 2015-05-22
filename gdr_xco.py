46e85af2ffdc3ac01f3f14e0dcff7928e1f98e84
low-sky <rosolowsky@gmail.com>
2015-05-21 23:35:42 -0600

    Some revisions to plotting code

--- gdr_xco.py
+++ gdr_xco.py
@@ -2,6 +2,9 @@ import emcee
 import numpy as np
 import scipy.stats as ss
 from scipy.special import erf
+import seaborn as sns
+import pandas as pd
+
 data = np.loadtxt('M33_sample.txt')
 wco, wco_err, whi,whi_err, dustsd = data[:,0],data[:,1],data[:,2],data[:,3],data[:,4]
 
@@ -33,18 +36,16 @@ def logprob_erf(p,wco,sigco,whi,sighi,dustsd):
 
 def logprob(p,wco,sigco,whi,sighi,dustsd):
     gdr,alphaCO,sigdust,Kdark = p[0],p[1],p[2],p[3]
-    a = 0.0146/gdr
+    a = 1/gdr
     b = alphaCO/gdr
-    c = 0
-#    c = Kdark/gdr
+    c = Kdark/gdr
     if np.any(p[0:2]<0):
         return -np.inf
     term1 = -0.5*(c+b*wco+a*whi-dustsd)**2/(sigdust**2+b**2*sigco**2+a**2*sighi**2)
     term2 = -0.5*np.log(((a**2/sigdust**2)+(1/sighi**2)))
     term3 = -0.5*np.log((sigdust**2*sigco**2*sighi**2))
     term4 = -0.5*np.log((sigdust**2+b**2*sigco**2+a**2*sighi**2)/(sigdust**2*sigco**2+a**2*sigco**2*sighi**2))
-    prior = ss.unform.logpdf(gdr/400,0,1)
-    print(np.sum(term1),np.sum(term2),np.sum(term3),np.sum(term4))
+    prior = ss.uniform.logpdf(gdr/400,0,1)
 
     return(np.sum(term1+term2+term3+term4)+prior)
 
@@ -57,7 +58,12 @@ p0[:,3] = ss.uniform.rvs(size=nwalkers)*10
 p0[:,2] = dustsd.mean()*(0.3+np.random.randn(nwalkers)*0.05)
 
 
-sampler = emcee.EnsembleSampler(nwalkers,ndim,logprob_erf,args=[wco,wco_err,whi,whi_err,dustsd])
+sampler = emcee.EnsembleSampler(nwalkers,ndim,logprob,args=[wco,wco_err,whi,whi_err,dustsd])
 pos,prob,state = sampler.run_mcmc(p0,300)
 sampler.reset()
 pos,prob,state = sampler.run_mcmc(p0,1000)
+frames = pd.DataFrame(sampler.flatchain,columns=['GDR','alphaCO','c','sigdust'])
+g = sns.JointGrid("GDR","alphaCO",frames,space=0)
+g.plot_marginals(sns.kdeplot, shade=True)
+g.plot_joint(sns.kdeplot, shade=True, cmap="PuBu", n_levels=40)
+plt.savefig('marginals.pdf')
